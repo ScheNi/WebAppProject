e456967967defa2f768bf5c38035f174bf116445
Nicolas Schelkens <schelkensnicolas@hotmail.com>
2015-11-14 17:25:31 +0100

    Adding basic user input + some styling

--- public/src/js/controllers/MainCtrl.js
+++ public/src/js/controllers/MainCtrl.js
@@ -1,5 +1,6 @@
 angular.module('flapperNews')
     .controller('MainCtrl', ['$scope', function ($scope) {
+
         $scope.posts = [
             {title: 'post 1', upvotes: 5},
             {title: 'post 2', upvotes: 2},
@@ -7,4 +8,20 @@ angular.module('flapperNews')
             {title: 'post 4', upvotes: 9},
             {title: 'post 5', upvotes: 1}
         ];
+
+        $scope.addPost = function(){
+            if(!$scope.title || $scope.title === '') { return; }
+            $scope.posts.push({
+                title: $scope.title,
+                link: $scope.link,
+                upvotes: 0
+            });
+            $scope.title = '';
+            $scope.link = '';
+        };
+
+        $scope.incrementUpvotes = function(post) {
+            post.upvotes += 1;
+        };
+
     }]);
