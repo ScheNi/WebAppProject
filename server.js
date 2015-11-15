a1573d185c363cc92baef6012c4d72865fd7f10a
Nicolas Schelkens <schelkensnicolas@hotmail.com>
2015-11-15 20:26:26 +0100

    Auth config, login & register route

--- server.js
+++ server.js
@@ -1,21 +1,23 @@
 // modules =================================================
 var express = require('express');
+var app = express();
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var logger = require('morgan');
 var path = require('path');
+var passport = require('passport');
 
 // configuration ===========================================
 // init database
 require('./app/models/Posts');
 require('./app/models/Comments');
+require('./app/models/Users');
+require('./config/passport');
 mongoose.connect('mongodb://localhost/news');
 
 //routes
 var index = require('./app/routes/index');
 var posts = require('./app/routes/posts');
-
-var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, './public/dist'));
@@ -25,6 +27,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, './public/dist')));
+app.use(passport.initialize());
 
 app.use('/', index);
 app.use('/', posts);
