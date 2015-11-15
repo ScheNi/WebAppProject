37105446de62ab3500007e8d8877c9808ae14e14
Nicolas Schelkens <schelkensnicolas@hotmail.com>
2015-11-15 17:11:39 +0100

    Post upvote (to the back-end)

--- public/src/js/services/PostsFactory.js
+++ public/src/js/services/PostsFactory.js
@@ -5,16 +5,23 @@ angular.module('flapperNews')
             posts: []
         };
 
-        o.getAll = function() {
-            return $api.get('/posts').success(function(data){
+        o.getAll = function () {
+            return $api.get('/posts').success(function (data) {
                 angular.copy(data, o.posts);
             });
         };
 
-        o.create = function(post) {
-            return $api.post('/posts', post).success(function(data){
+        o.create = function (post) {
+            return $api.post('/posts', post).success(function (data) {
                 o.posts.push(data);
             });
+        };
+
+        o.upvote = function (post) {
+            return $api.put('/posts/' + post._id + '/upvote')
+                .success(function (data) {
+                    post.upvotes += 1;
+                });
         };
 
         return o;
