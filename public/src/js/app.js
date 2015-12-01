ebdc2e7b51d1ebaa98fb4b872f7a1fec2d6046dc
Nicolas Schelkens <schelkensnicolas@hotmail.com>
2015-12-01 19:02:21 +0100

    custom directive wip

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,4 +1,4 @@
-angular.module('flapperNews', ['ui.router'])
+angular.module('flapperNews', ['ui.router', 'ngAnimate'])
     .config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
 
         $stateProvider
--- public/src/js/controllers/MainCtrl.js
+++ public/src/js/controllers/MainCtrl.js
@@ -1,6 +1,23 @@
 angular.module('flapperNews')
     .controller('MainCtrl', ['$scope', '$posts', '$auth', function ($scope, $posts, $auth) {
 
+        $scope.images = [{
+            src: 'img1.png',
+            title: 'Pic 1'
+        }, {
+            src: 'img2.jpg',
+            title: 'Pic 2'
+        }, {
+            src: 'img3.jpg',
+            title: 'Pic 3'
+        }, {
+            src: 'img4.png',
+            title: 'Pic 4'
+        }, {
+            src: 'img5.png',
+            title: 'Pic 5'
+        }];
+
         $scope.posts = $posts.posts;
         $scope.isLoggedIn = $auth.isLoggedIn;
 
