132f3c7127fef47091ad3c46d4a133c0e9f6d22f
Nicolas Schelkens <schelkensnicolas@hotmail.com>
2015-12-01 20:13:29 +0100

    registration: check if user already exists

--- app/routes/auth.js
+++ app/routes/auth.js
@@ -9,30 +9,39 @@ router.post('/register', function (req, res, next) {
         return res.status(400).json({message: 'Please fill out all fields'});
     }
 
-    var user = new User();
+    User.findOne({'username': req.body.username}, function (err, user) {
+        console.log(user);
+        console.log(err);
+        if (user) {
+            return res.status(400).json({message: 'There is already an account with this username'});
+        }
+        var user = new User();
 
-    user.username = req.body.username;
+        user.username = req.body.username;
 
-    user.setPassword(req.body.password)
+        user.setPassword(req.body.password)
 
-    user.save(function (err) {
+        user.save(function (err) {
+            if (err) {
+                return next(err);
+            }
+            return res.json({token: user.generateJWT()})
+        });
+    });
+
+});
+
+router.post('/login', function (req, res, next) {
+    if (!req.body.username || !req.body.password) {
+        return res.status(400).json({message: 'Please fill out all fields'});
+    }
+
+    passport.authenticate('local', function (err, user, info) {
         if (err) {
             return next(err);
         }
 
-        return res.json({token: user.generateJWT()})
-    });
-});
-
-router.post('/login', function(req, res, next){
-    if(!req.body.username || !req.body.password){
-        return res.status(400).json({message: 'Please fill out all fields'});
-    }
-
-    passport.authenticate('local', function(err, user, info){
-        if(err){ return next(err); }
-
-        if(user){
+        if (user) {
             return res.json({token: user.generateJWT()});
         } else {
             return res.status(401).json(info);
